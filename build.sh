a719f26decdfbbec295d6f96aa8fa487c62f75fb
v81d <v81d@proton.me>
2026-01-14 22:16:09 -0500

    build: use foundry for run script

--- build.sh
+++
@@ -1,4 +0,0 @@
-meson setup _build --prefix="$PWD/_build/install" --reconfigure
-ninja -C _build
-ninja -C _build install
-GSETTINGS_SCHEMA_DIR=_build/data ./_build/src/autopilot
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+foundry init
+foundry run
